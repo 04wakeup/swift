8ccf49cc103d1495d58f11b3772dd38d8e383f53
04wakeup <dojun849@gmail.com>
2020-01-29 02:47:35 -0800

    xxx

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by jonghyun choi on 2020-01-09.
+//  Copyright © 2020 jonghyun choi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
