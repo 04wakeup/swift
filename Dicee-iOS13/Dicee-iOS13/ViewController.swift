f83fd2e6a9a08b904dbb407dcda19abefc8210a1
04wakeup <dojun849@gmail.com>
2020-01-29 02:23:34 -0800

    init

---
+++ Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][leftDiceNumber]
+        diceImageView2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ][rightDiceNumber]
+        leftDiceNumber = (leftDiceNumber + 1) % 6
+        rightDiceNumber = (rightDiceNumber - 1) % 6
+        if (rightDiceNumber < 0){
+            rightDiceNumber = 5
+        }
+        print("the current value is \(leftDiceNumber).")
+    }
+    
+
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+//This is a comment
+
+//var a = 5
+//var b = 8
+//
+//print("The value of a is \(a)")
+//print(b)
+
+var numbers = [45, 73, 195, 53]
+var computedNumbers = [numbers[0] * numbers[1],numbers[1] * numbers[2],numbers[2] * numbers[3],numbers[3] * numbers[0] ]
+
+print(computedNumbers)
