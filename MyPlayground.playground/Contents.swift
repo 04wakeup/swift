8fb4894d0a3d67e27cbc5ce4739772f6e90b3bd7
james <dojun849@gmail.com>
2020-02-04 05:03:01 -0800

    hh

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -6,7 +6,16 @@
 //print("The value of a is \(a)")
 //print(b)
 
-var numbers = [45, 73, 195, 53]
-var computedNumbers = [numbers[0] * numbers[1],numbers[1] * numbers[2],numbers[2] * numbers[3],numbers[3] * numbers[0] ]
+//var numbers = [45, 73, 195, 53]
+//var computedNumbers = [numbers[0] * numbers[1],numbers[1] * numbers[2],numbers[2] * numbers[3],numbers[3] * numbers[0] ]
+//
+//print(computedNumbers)
 
-print(computedNumbers)
+// example: random
+let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+//The number of letters in alphabet equals 26
+
+var password = alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()!  
+print(password)
+
