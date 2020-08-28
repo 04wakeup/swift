5198ea51ad9e0cb4dbae05eb9135879bbd1b34f5
04wakeup <dojun849@gmail.com>
2020-08-28 11:17:23 -0700

    start again

--- Destini-iOS13_V1/Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13_V1/Destini-iOS13/Controller/ViewController.swift
@@ -13,31 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
-    
-//    var questionNumber = 0
-//    var nextQuestionIndex = 0
-    
-//    var stories = [
-//        Story(title: "You see a fork in the road.",
-//              choice1: "Take a left",
-//              choice2: "Take a right!.",
-//              leftChoice: 1,
-//              rightChoice: 2
-//        ),
-//        Story(title: "You see a tiger.",
-//              choice1: "Shout for help",
-//              choice2: "Play dead!.",
-//              leftChoice: 0,
-//              rightChoice: 0
-//            
-//        ),
-//        Story(title: "You find a treasure chest.",
-//              choice1: "Open it",
-//              choice2: "Check for traps",
-//              leftChoice: 0,
-//              rightChoice: 0
-//        )
-//    ]
+     
     var storyBrain = StoryBrain()
     
     override func viewDidLoad() {
