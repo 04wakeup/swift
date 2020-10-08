d84e57ffb6fbde8e25f16d7078b26e5b2842a927
04wakeup <dojun849@gmail.com>
2020-10-08 00:49:08 -0700

    UITableView added

---
+++ Flash-Chat/Flash Chat iOS13/Constants.swift
@@ -0,0 +1,28 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Jonghyun Choi on 2020-10-07.
+//
+
+struct K {
+    static let appName = "🟡Nice Chat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
---
+++ Flash-Chat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Jonghyun Choi on 2020-10-08.
+//
+
+import Foundation
+
+struct Message{
+    let sender: String
+    let body: String
+    
+}
